a12672837ca400b2c797fa9ffa6788f6bb08ff5f
André Jálisson <andrejalisson@gmail.com>
2024-11-25 11:35:37 -0300

    Template Insuba

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(SiteController::class)->group(function () {
+    Route::get('/', 'home');
+    Route::get('/Sobre', 'sobre');
+    Route::get('/Servicos', 'servicos');
+    Route::get('/Assistencia', 'assistencia');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
